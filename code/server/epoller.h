74c02328b6e9f9e2505c1bd0365afff6c3d2d3a4
CiroLong <longruoqin@gmail.com>
2025-05-18 00:33:28 +0800

    dev: epoller done

---
+++ code/server/epoller.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <sys/epoll.h> //epoll_ctl()
+#include <fcntl.h>     // fcntl()
+
+#include <unistd.h> //close()
+#include <assert.h>
+#include <vector>
+#include <errno.h>
+
+// Epoller 封装底层的epoll实现
+class Epoller
+{
+public:
+    explicit Epoller(int maxEvent = 1024);
+
+    ~Epoller();
+
+    bool AddFd(int fd, uint32_t events);
+
+    bool ModFd(int fd, uint32_t events);
+
+    bool DelFd(int fd);
+
+    int Wait(int timeoutMS = -1);
+
+    int GetEventFd(size_t i) const;
+
+    uint32_t GetEvents(size_t i) const;
+
+private:
+    int epollFd_;
+    std::vector<struct epoll_event> events_;
+};
